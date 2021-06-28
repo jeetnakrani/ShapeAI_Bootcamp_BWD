a94e2d255f56ece806786ca3d22b6d843a0fd0e9
jeetnakrani <67156269+jeetnakrani@users.noreply.github.com>
2021-06-28 14:57:29 +0530

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Info() {
+  return (
+    <div className="note">
+      <h1>Javascript and ReactJs</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Magni
+        pariatur, nesciunt doloribus ab unde provident sed ipsum sapiente, nisi
+        aliquam similique. 
+      </p>
+    </div>
+  );
+}
+export default Info;
